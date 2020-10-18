37adc755e224d49e369e9ff06619b57e9c855ce3
lidor <lidorfadida93@gmail.com>
2020-10-18 04:26:52 +0300

    Test

--- Restt/View/ToppingsView.swift
+++ Restt/View/ToppingsView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ToppingsView: UIView {
-
+    
     @IBOutlet weak var btCorn: UIButton!
     @IBOutlet weak var btCheese: UIButton!
     @IBOutlet weak var btOlives: UIButton!
